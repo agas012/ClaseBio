a433774314be8d003e30dc13f096e07ea0235b5a
Alfonso Gastelum-Strozzi <alfonso.gastelum@comunidad.unam.mx>
2022-10-19 08:25:16 -0500

    new steps

--- ClaseBio/biostata.py
+++ ClaseBio/biostata.py
@@ -2,6 +2,18 @@ from ast import Lambda
 import numpy as np
 import pandas as pd
 import math
+import scipy.stats as ss
+
+def is_categorical(array_like):
+    """
+    Test if the array is categorical.
+
+    Parameters
+    ----------
+    array_like : pandas.arrays
+        Input array to be tested
+    """
+    return array_like.dtype.name == 'category'
 
 data_in = data_I.copy()
 colgrouped = 'Outcome'
@@ -23,9 +35,9 @@ def populationtest(data_in, colgrouped, valueslist, outpath, filename):
         list of columns to be tested
     """
     #list of significance
-    data_sig = pd.DataFrame(data = {'Name': data_in.columns, 'statistic': np.zeros(data_in.shave[1]), 'p': np.zeros(data_in.shave[1])})
+    data_sig = pd.DataFrame(data = {'Name': data_in.columns, 'statistic': np.zeros(data_in.shape[1]), 'p': np.zeros(data_in.shape[1])})
 
-    #check of type of variabel per column
+    #check of type of variabel per column check for categorical
     data_in.loc[:, data_in.dtypes == 'object'] =\
         data_in.select_dtypes(['object']).apply(lambda x: x.astype('category'))
     data_in.loc[:, data_in.isin([0,1]).all()] = data_in.loc[:, data_in.isin([0,1]).all()].apply(lambda x: x.astype('category'))
@@ -49,3 +61,67 @@ def populationtest(data_in, colgrouped, valueslist, outpath, filename):
         CQ.loc['Totales', coltitle] = CQ.loc['Totales', coltitle].astype(str)
         strval = CQ.loc['Totales', coltitle] + valn
         CQ.loc['Totales', coltitle] = strval.iloc[0,0]
+    coltitle ='Totales'
+    d = {'per': [(CQ.loc['Totales',coltitle]/CQ.loc['Totales','Totales'])*100.00]}
+    valn = pd.DataFrame(d)
+    valn = valn.applymap(lambda x: " ({:.2f})".format(x))
+    CQ.loc['Totales',coltitle] = CQ.loc['Totales',coltitle].astype(str)
+    strval = CQ.loc['Totales',coltitle] + valn
+    CQ.loc['Totales',coltitle] = strval.iloc[0,0]
+
+    for cols in colnames:
+        if(is_categorical(data_in[cols])):
+            ctableT = pd.crosstab(data_in[cols],data_in[colgrouped],margins=True)
+            ctableT = ctableT.drop('All')
+            if(ctableT.shape[0]>1):
+                ctablen = pd.crosstab(data_in[cols],data_in[colgrouped], normalize='columns',margins=True)*100.00
+                ctablen = ctablen.applymap(lambda x: " ({:.2f})".format(x))
+                stat, pvalue, dof, expected = ss.chi2_contingency(ctableT.loc[:,valueslist])
+                ctableT = ctableT.astype(str)
+                ctableT = ctableT + ctablen
+                ctableT.rename(columns={'All':'Totales'}, inplace = True)
+                data_sig.loc[data_sig.Name==cols,'p'] = pvalue
+                data_sig.loc[data_sig.Name==cols,'statistic'] = stat
+                if((ctableT.index == 0).any()):
+                    ctableT = ctableT.drop(0)
+                    ctableT.rename({1:cols}, inplace = True) 
+                if(pvalue < 0.001):
+                    ctableT.loc[ctableT.index[0], 'p'] = '<0.001'
+                elif(pvalue < 0.01):
+                    ctableT.loc[ctableT.index[0], 'p'] = '<0.01'
+                else:
+                    ctableT.loc[ctableT.index[0], 'p'] = "{:.3f}".format(pvalue) 
+                ctableT = ctableT.dropna()
+            else:
+                ctableT.rename(columns={'All':'Totales'}, inplace = True) 
+                ctableT.rename(index={ ctableT.index[0]: cols }, inplace = True)
+                data_sig.loc[data_sig.Name==cols,'p'] = math.nan
+                data_sig.loc[data_sig.Name==cols,'statistic'] = math.nan
+                for col in ctableT.columns:
+                    ctableT[col].values[:] = 0
+                ctableT.loc[ctableT.index[0], 'p'] = 'NaN'
+        else:
+            x=[]
+            dataset_numeric = pd.concat([data_in[cols], data_in[colgrouped]], axis=1)
+            for coltitle in valueslist:
+                x.append(dataset_numeric.loc[dataset_numeric[colgrouped] == coltitle,cols])
+            correlation, pvalue = ss.kruskal(*x)
+            ctableT = dataset_numeric.groupby(colgrouped).mean().T
+            ctableT.columns = ctableT.columns.tolist()
+            ctableT['Totales'] = dataset_numeric[cols].mean()
+            ctableT = ctableT.applymap(lambda x: "{:.2f}".format(x))
+            ctabled = dataset_numeric.groupby(ColGrouped).std().T
+            ctabled.columns = ctabled.columns.tolist()
+            ctabled['Totales'] = dataset_numeric[cols].std()
+            ctabled = ctabled.applymap(lambda x: " ({:.2f})".format(x))
+            ctableT = ctableT + ctabled
+            data_sig.loc[data_sig.Name==cols,'p'] = pvalue
+            data_sig.loc[data_sig.Name==cols,'statistic'] = correlation
+            if(pvalue < 0.001):
+                ctableT.loc[ctableT.index[0], 'p'] = '<0.001'
+            elif(pvalue < 0.01):
+                ctableT.loc[ctableT.index[0], 'p'] = '<0.01'
+            else:
+                ctableT.loc[ctableT.index[0], 'p'] = "{:.3f}".format(pvalue)
+        CQ  =  pd.concat([CQ, ctableT])
+    CQ = CQ.replace(np.nan,'',regex = True)
